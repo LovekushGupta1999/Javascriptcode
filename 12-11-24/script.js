837c06fb0fd012134ed4d092667f4c78a9b5e707
lovekushgupta1999 <guptalovekush64@gmail.com>
2024-11-12 22:10:49 +0530

    javascript code basic

---
+++ 12-11-24/script.js
@@ -0,0 +1,13 @@
+// alert("hello world"); 
+var name="lovekush";
+var age=24;
+console.log(age);
+document.write(name);
+
+var eligible=true;
+var isEligible=true; //camelCase
+console.log(name);
+console.log(typeof(name));
+var data=null;
+
+console.log(typeof(null));
