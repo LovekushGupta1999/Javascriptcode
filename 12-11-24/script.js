c27da9e12bda1770aa91bbe7f0defb69c0c9c458
lovekushgupta1999 <guptalovekush64@gmail.com>
2024-11-15 21:59:32 +0530

    javascript basics

--- 12-11-24/script.js
+++ 12-11-24/script.js
@@ -1,13 +1,53 @@
-// alert("hello world"); 
-var name="lovekush";
-var age=24;
-console.log(age);
-document.write(name);
+// // alert("hello world"); 
+// var name="lovekush";
+// var age=24;
+// console.log(age);
+// document.write(name);
 
-var eligible=true;
-var isEligible=true; //camelCase
-console.log(name);
-console.log(typeof(name));
-var data=null;
+// var eligible=true;
+// var isEligible=true; //camelCase
+// console.log(name);
+// console.log(typeof(name));
+// var data=null;
 
-console.log(typeof(null));
+// console.log(typeof(null));
+
+// document.getElementById("demo").innerHTML = "Hello world";
+// document.getElementById("demo").innerHTML="welcome to javascript";
+
+// changing the inner html -->
+var h1=document.getElementById("demo");
+h1.innerHTML="hello world";
+
+// changing the css of element --> 
+h1.style.color= "red";
+h1.style.backgroundColor= "yellow";
+h1.style.paddingTop="300px";
+h1.style.border="5px solid black";
+h1.style.fontSize="200px";
+h1.style.height="100%";
+var button=document.getElementById("btn");
+button.innerHTML="Show";
+button.style.background="black";
+button.style.color="white";
+button.style.border="solid";
+button.style.borderRadius="20px";
+button.style.fontSize="100px";
+
+
+// make a h1 ,p and button and add innerhtml to it 
+
+
+var heading=document.getElementById("demo");
+heading.innerHTML=" Do You Work Everyday";
+
+var name="John Doe";
+var age=34;
+// concatination
+console.log(name + " " + age);
+heading.innerHTML=name+ " "+ age;
+//to get value from user
+var userName=prompt("enter your name");
+var userAge=prompt("enter your age");
+
+heading.innerHTML=userName+ " "+userAge;
