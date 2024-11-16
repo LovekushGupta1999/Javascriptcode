2fd1266e53036c2ea20965a601ac546b1d2f3bd2
lovekushgupta1999 <guptalovekush64@gmail.com>
2024-11-16 20:58:30 +0530

    some concepts in javascript

--- 12-11-24/script.js
+++ 12-11-24/script.js
@@ -15,39 +15,58 @@
 // document.getElementById("demo").innerHTML = "Hello world";
 // document.getElementById("demo").innerHTML="welcome to javascript";
 
-// changing the inner html -->
-var h1=document.getElementById("demo");
-h1.innerHTML="hello world";
+// // changing the inner html -->
+// var h1=document.getElementById("demo");
+// h1.innerHTML="hello world";
 
 // changing the css of element --> 
-h1.style.color= "red";
-h1.style.backgroundColor= "yellow";
-h1.style.paddingTop="300px";
-h1.style.border="5px solid black";
-h1.style.fontSize="200px";
-h1.style.height="100%";
-var button=document.getElementById("btn");
-button.innerHTML="Show";
-button.style.background="black";
-button.style.color="white";
-button.style.border="solid";
-button.style.borderRadius="20px";
-button.style.fontSize="100px";
+// h1.style.color= "red";
+// h1.style.backgroundColor= "yellow";
+// h1.style.paddingTop="300px";
+// h1.style.border="5px solid black";
+// h1.style.fontSize="200px";
+// h1.style.height="100%";
+// var button=document.getElementById("btn");
+// button.innerHTML="Show";
+// button.style.background="black";
+// button.style.color="white";
+// button.style.border="solid";
+// button.style.borderRadius="20px";
+// button.style.fontSize="100px";
 
 
 // make a h1 ,p and button and add innerhtml to it 
 
 
-var heading=document.getElementById("demo");
-heading.innerHTML=" Do You Work Everyday";
+// var heading=document.getElementById("demo");
+// heading.innerHTML=" Do You Work Everyday";
 
-var name="John Doe";
-var age=34;
-// concatination
-console.log(name + " " + age);
-heading.innerHTML=name+ " "+ age;
-//to get value from user
+// var name="John Doe";
+// var age=34;
+// // concatination
+// console.log(name + " " + age);
+// heading.innerHTML=name+ " "+ age;
+// //to get value from user
+// var userName=prompt("enter your name");
+// var userAge=prompt("enter your age");
+
+// heading.innerHTML=userName+ " "+userAge;
+//  var txtcolor=prompt(" Enter your color");
+// h1.style.color=txtcolor;
+// var bgcolor=prompt(" Enter your Bac kgraound color");
+// h1.style.backgroundColor=bgcolor;
+
+// get 
+
+// var  Myimage= document.getElementById("Myimage");
+// Myimage.src="1.img" ;
+
 var userName=prompt("enter your name");
-var userAge=prompt("enter your age");
+var h1=document.getElementById("demo");
 
-heading.innerHTML=userName+ " "+userAge;
+h1.innerHTML=userName;
+h1.style.color= "brown";
+h1.style.backgroundColor= "gray";
+h1.style.textAlign="center";
+h1.style.fontSize="80px";
+
---
+++ 16-11-24/scirpt.js
@@ -0,0 +1,19 @@
+// var num1=10;
+// var num2=10;
+// if(num1==num2){
+//     alert("both are equal");
+
+// }
+// else{
+//     alert("not equal");
+// }
+//==compare only value, === compare value and datatype
+var name="lovekush";
+var age="24";
+var course="java";
+
+
+var h1=document.getElementById("demo");
+h1.innerHTML= name + age + course;
+alert(name+age+course);
+console.log(name+age+course);
