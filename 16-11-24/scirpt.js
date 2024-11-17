42faa84bc8aadff2d60bb8dd67dfe1e1a8a8c2a2
lovekushgupta1999 <guptalovekush64@gmail.com>
2024-11-17 20:13:47 +0530

    practice coding

--- 16-11-24/scirpt.js
+++ 16-11-24/scirpt.js
@@ -8,12 +8,37 @@
 //     alert("not equal");
 // }
 //==compare only value, === compare value and datatype
+// var name="lovekush";
+// var age="24";
+// var course="java";
+
+
+// var h1=document.getElementById("demo");
+// h1.innerHTML= name + age + course;
+// alert(name+age+course);
+// console.log(name+age+course);
+
 var name="lovekush";
 var age="24";
+var city="bhopal"
 var course="java";
+var country="INDIA";
+console.log(name+age+city+course+country);
 
+var course="java";
+var fees="50000";
+var details=course+" "+fees;
+alert(details);
 
-var h1=document.getElementById("demo");
-h1.innerHTML= name + age + course;
-alert(name+age+course);
-console.log(name+age+course);
+var lastName="Gupta";
+var hobby="Playing cricket"
+
+ h1=document.getElementById("demo");
+ h1.innerHTML= name + lastName + hobby;
+
+var userName=prompt("enter your name");
+var userAge=prompt("enter your age");
+alert(userName+userAge);
+console.log(userName+userAge);
+h1.innerHTML=userName+userAge;
+
