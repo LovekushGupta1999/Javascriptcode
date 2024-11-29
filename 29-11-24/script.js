dba8558a28490c54517b6040dfaa5e231174a7c4
lovekushgupta1999 <guptalovekush64@gmail.com>
2024-11-29 21:50:08 +0530

    javascript

---
+++ 29-11-24/script.js
@@ -0,0 +1,18 @@
+let name="lovekush";
+function show(){
+    console.log(name);
+    document.getElementById("demo").innerHTML=name;
+    alert(name);
+}
+
+const show1= function(){
+    console.log(name);
+    document.getElementById("demo").innerHTML=name;
+    alert(name);
+}
+
+const show2=()=>{
+    console.log(name);
+    document.getElementById("demo").innerHTML=name;
+    alert(name);
+}
