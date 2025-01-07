f453185bd95450f2678202748de445e57617eb93
lovekushgupta1999 <guptalovekush64@gmail.com>
2025-01-07 19:01:44 +0530

    json

--- 4-1-25/script.js
+++ 4-1-25/script.js
@@ -8,9 +8,10 @@ async function showtable(){
         return `
         <tr>
         <td>${e.id}</td>
-        <td>${e.name}</td>
+        <td>${e.name}</td> 
         <td>${e.city}</td>
         <td><button onclick="mydelete(${e.id})">Delete</button></td>
+        <td><button onclick="edit(${e.id})">Edit</button></td>
         </tr>
 
         `
@@ -22,13 +23,34 @@ async function showtable(){
 }
 showtable();
 
+
+//data Deletion
 function mydelete(id){
     fetch(`http://localhost:3000/student/${id}`,{method: 'Delete'}).then(r=>alert("deleted!"))
 }
-// function getdata(){
+
+
+//data insertion
+function insert_data(){
+    let data={
+        id: document.querySelector("#input1").value,
+        name: document.querySelector("#input2").value,
+        city: document.querySelector("#input3").value
+    }
+
+fetch("http://localhost:3000/student",{
+    method: "POST",
+    headers:{
+        'content-type':'application/json'
+    },
+    body: JSON.stringify(data)
+}).then(r=>alert("date inserted"))
+}
+// function setdata(){
 //     let data={
 //         id: document.getElementById("input1").value,
 //         name: document.getElementById("input2").value
+//          city : document.getElementById("input3").value
 
 //     }
 //     const fs =require("fs");
@@ -44,3 +66,35 @@ function mydelete(id){
 //         console.log("new data added")
 //     });
 // }
+
+//data edition
+
+async function edit(id){
+    let res =await fetch(`http://localhost:3000/student/${id}`);
+    let data =await res.json();
+    let edit_frm=`
+    <input type="text" value="${data.id}" id="id1"><br>
+    <input type="text" value="${data.name}" id="name1"><br>
+    <input type="text" value="${data.city}" id="city1"><br>
+    <input type="submit" value="Update" onclick="myupdate('${id}')">
+    `
+    document.querySelector("#editform").innerHTML=edit_frm
+
+}
+
+//updation
+
+function myupdate(id){ 
+    let update_data={
+        id: document.querySelector("#id1").value,
+        name: document.querySelector("#name1").value,
+        city: document.querySelector("#ciry1").value
+    }
+    fetch(`http://localhost:3000/student/${id}`,{
+        method:"PUT",
+        headers:{
+            "content-type":"application/json"
+        },
+        body: JSON.stringify(update_data)
+    }).then(r=>alert("updated.........!!"))
+}
